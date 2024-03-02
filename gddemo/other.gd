4d350428ce853371a1581a98e46b3d2e0a4e14c5
Tom <tom@contextfree.info>
2024-03-02 05:44:09 -0700

    Explore gd const and py pointers

--- gddemo/other.gd
+++ gddemo/other.gd
@@ -6,9 +6,9 @@ static func something():
 	try_arrays()
 	var gabber := Gabber.new()
 	gabber.gab()
-	gabber.something = gabber
-	#gabber.something = weakref(gabber)
-	#gabber.something = 5
+	gabber.anything = gabber
+	#gabber.anything = weakref(gabber)
+	#gabber.anything = 5
 	#gabber.free()
 	#gabber.free()
 	#gabber.gab()
@@ -20,14 +20,17 @@ static func _static_init():
 
 
 static func try_arrays():
-	var something := [3, 4] as Array[int]
-	#something[0] = "hi"
-	#var blah: Array = something
+	const numbers: Array[int] = [3, 4]
+	# var numbers := [3, 4] as Array[int]
+	numbers.append(5)
+	#numbers[0] = "hi"
+	#var blah: Array = numbers
 	#blah[0] = "hi"
+	print("numbers: %s" % [numbers])
 
 
 class Gabber: # extends Object:
-	var something: Variant
+	var anything: Variant
 
 	func gab():
 		print("Gabbing now")
--- pydemo/__main__.py
+++ pydemo/__main__.py
@@ -1,8 +1,10 @@
+import ctypes as c
 import pprint as pp
 from . import moments as m
 
 
 def main():
+    # print(c.c_char_p(id(0)).value)
     pp.pp(m.moments)
 
 
