38c071323e8fbb65f4b4b4732a460f6b516e7144
Tom <tom@contextfree.info>
2024-03-01 20:50:42 -0700

    Explore types, running in editor

--- gddemo/main.gd
+++ gddemo/main.gd
@@ -1,16 +1,23 @@
+#@tool
+#extends EditorScript
 extends MainLoop
-# extends SceneTree
+#extends SceneTree
 
 
+#func _run():
 func _init():
-    print("Hi there!")
-    # preload("res://gddemo/other.gd")
-    var other := load("res://gddemo/other.gd")
-    print(other)
-    # other.free()
-    other.something()
-    # Other.something()
-    # quit()
+	print("Hi there!")
+	#preload("res://gddemo/other.gd")
+	var other := load("res://gddemo/other.gd")
+	print(other)
+	#other.free()
+	other.something()
+	#Other.something()
+	explore_types()
+	#quit()
 
 
-# var other := preload("res://gddemo/other.gd")
+func explore_types():
+	var name: String
+	print(name.length())
+	print(not name)
