fb47c452227805124595da5b006334e0e09440a6
Tom <tom@contextfree.info>
2024-03-01 20:59:09 -0700

    Explore memory and more types

--- gddemo/other.gd
+++ gddemo/other.gd
@@ -2,8 +2,24 @@ class_name Other
 
 
 static func something():
-    print("more")
+	print("more")
+	var gabber := Gabber.new()
+	gabber.gab()
+	gabber.something = gabber
+	#gabber.something = weakref(gabber)
+	#gabber.something = 5
+	#gabber.free()
+	#gabber.free()
+	#gabber.gab()
+	#gabber = 5
 
 
 static func _static_init():
-    print("Static init")
+	print("Static init")
+
+
+class Gabber: # extends Object:
+	var something: Variant
+
+	func gab():
+		print("Gabbing now")
